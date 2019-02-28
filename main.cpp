d00da47e07757639514a9de5e1b94b368f7a25d9
James <1036603@students.wits.ac.za>
2019-02-28 16:14:18 +0200

    Added print and random generate method

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-template<typename T> vector<vector<T>> TDSquareMatrix(int dimension)
+template<typename T> vector<vector<T>> _2DSquareMatrix(int dimension)
 //Template 2D square matrix
 {
     vector<vector<T>> temp2DMatrix;
@@ -18,10 +18,35 @@ template<typename T> vector<vector<T>> TDSquareMatrix(int dimension)
     return temp2DMatrix;
 }
 
+void printMatrix(const auto* inMatrix)
+{//Prints an NxN matrix
+    auto N = inMatrix->size();
+    for(auto i = 0; i<N; i++)
+    {
+        for(auto j = 0; j<N; j++)
+        {
+            cout<<myMatrix.at(i).at(j)<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void PopulateRandomMatrux(auto* inMatrix)
+{//Populate Matrix with random values between 0 and 9
+    auto N = inMatrix->size();
+    for(auto i = 0; i<N; i++)
+    {
+        for(auto j = 0; j<N; j++)
+        {
+            myMatrix.at(i).at(j) = rand()%10;
+        }
+    }
+}
+
 
 
 int main(int argc, char **argv)
 {
-	auto myNm = TDSquareMatrix<char>(5);
+    
 	return 0;
 }
