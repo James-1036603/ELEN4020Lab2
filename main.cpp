2eac150fb57ee10a7ab70f2ef8ab36f2741f8b54
James <1036603@students.wits.ac.za>
2019-02-28 15:38:09 +0200

    Template 2D vector

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename T> vector<vector<T>> TDSquareMatrix(int dimension)
+//Template 2D square matrix
+{
+    vector<vector<T>> temp2DMatrix;
+    for(auto i = 0; i < dimension; i++)
+    {   
+        vector<T> tempRow(dimension);
+        temp2DMatrix.push_back(tempRow);
+    }
+    
+    return temp2DMatrix;
+}
+
+
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	auto myNm = TDSquareMatrix<char>(5);
 	return 0;
 }
