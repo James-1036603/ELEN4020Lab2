49ae60519b353f80c02b2a292f09400458336424
James <1036603@students.wits.ac.za>
2019-03-05 11:57:01 +0200

    Chunk Transpose- Transpose and print

--- main.cpp
+++ main.cpp
@@ -11,11 +11,11 @@ template<typename T> vector<vector<T>> _2DSquareMatrix(int dimension)
 {
     vector<vector<T>> temp2DMatrix;
     for(auto i = 0; i < dimension; i++)
-    {   
+    {
         vector<T> tempRow(dimension);
         temp2DMatrix.push_back(tempRow);
     }
-    
+
     return temp2DMatrix;
 }
 
@@ -47,68 +47,78 @@ void PopulateRandomMatrix(auto* inMatrix)
 void SerialMatrixTranspose(auto* inMatrix)
 //Transposes a NxN matrix without any threading
 {
-    auto N = inMatrix->size();    
+
+    auto N = inMatrix->size();
     for(auto i = 0; i < N; i++)
     {
         for(auto j = i; j < N; j++){
             auto tempVar = inMatrix->at(i).at(j);
             inMatrix->at(i).at(j) = inMatrix->at(j).at(i);
             inMatrix->at(j).at(i) = tempVar;
-            cout<<endl;
         }
     }
 }
 
 
-auto transposeMatrixByChunks(auto* matrix, size_t chunkSize)
-{//Transpose a matrix by dividing it into specified sized chunks. The matrix will have to be a factor of the chunk size to chunk evenly
+void transposeMatrixByChunks(auto* matrix, size_t chunkSize)
+{//Transpose a matrix by dividing it into specified sized chunks. The matrix will have to be a factor of the chunk size to chunk evenly. THIS DOES NOT RETURN A MTRIX> MERELY TRANSPOSES AND DISPLAYS
+//Issue is you can't return the new smaller chuncked matrix to the others without a printing method
     if(matrix->size()%chunkSize==0)
     {
-        
+
         auto myTemp = _2DSquareMatrix<vector<vector<int>>>(chunkSize);
         auto temp2DMatrixA = _2DSquareMatrix<int>(chunkSize);
-        
-        
+
+
         for(auto i = 0; i < chunkSize; i++)//Cols
         {//Assign the values of the matrix to the temp matrices
-        
+
             for(auto j = 0; j < chunkSize; j++)//Rows
             {
-                
+
                 for(auto k = 0; k < chunkSize; k++)
                 {
-                    
+
                     for(auto l = 0; l < chunkSize; l++)
                     {
                         //Iterate along each row and col of the passed matrix to a temp matrix. Assign that temp matrix to the big larger matrix once it reaches an iteration of chunkSize
-                        temp2DMatrixA.at(k).at(l) = matrix->at(k+(i*chunkSize)).at(l+(j*chunkSize));                        
+                        temp2DMatrixA.at(k).at(l) = matrix->at(k+(i*chunkSize)).at(l+(j*chunkSize));
                     }
                 }
                 myTemp.at(i).at(j) = temp2DMatrixA;
             }
-                
+
         }
-        
-        //print the matrix
+
+        //Transpose the matrix by first transposing the  smaller matrices and then the larger
         for(auto i = 0; i<chunkSize; i++)
             for(auto j = 0; j<chunkSize; j++)
+                SerialMatrixTranspose(&myTemp.at(i).at(j));
+        SerialMatrixTranspose(&myTemp);
+
+
+        for(auto i = 0; i<chunkSize; i++)
+            for(auto j = 0; j<chunkSize; j++){
                 printMatrix(&myTemp.at(i).at(j));
+                cout<<endl;
+              }
     }
-    return 0;
+
 }
 
 
 int main(int argc, char **argv)
 {
-    
+
     srand(time(NULL));
-    auto my2dM = _2DSquareMatrix<int>(4);
+    auto my2dM = _2DSquareMatrix<int>(9);
     PopulateRandomMatrix(&my2dM);
     printMatrix(&my2dM);
     cout<<endl;
-    transposeMatrixByChunks(&my2dM,2);
-    
-    
-    
+    transposeMatrixByChunks(&my2dM,3);
+    //printMatrix(&my2dM);
+
+
+
 	return 0;
 }
